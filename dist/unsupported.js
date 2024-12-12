10a0e4edbf63866f66928e7d9ffa6e3383ec2d25
gitcobra <gitpython@outlook.jp>
2024-12-12 23:16:25 +0900

    v1.7.22

--- dist/unsupported.js
+++ dist/unsupported.js
@@ -12,7 +12,8 @@
         app.innerHTML =
           '<p>Sorry, <strong>AVIF to JPEG "Offline" Batch Converter</strong> is not working properly.</p>'+
           '<p>If you can still see this text even after reloading, perhaps this page does not support your web browser.</p>'+
-          '<p>Please consider using the latest version of the modern web browser.</p>'
+          '<p>Please consider using the latest version of the modern web browser.</p>'+
+          '<p><button onclick="location.reload()">Reload</button></p>'
       }
       disclaimer = null;
       app = null;
