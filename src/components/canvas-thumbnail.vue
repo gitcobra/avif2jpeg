7cf4bf4628399516341369936230c50179b18396
gitcobra <gitpython@outlook.jp>
2025-09-21 13:42:19 +0900

    fix: thumbnail aspect ratio issue in file list on Firefox

--- src/components/canvas-thumbnail.vue
+++ src/components/canvas-thumbnail.vue
@@ -23,7 +23,7 @@ const slots = defineSlots<{
 
 
 // variables
-const USE_ImageBitmap = (typeof window.createImageBitmap === 'function');
+const USE_ImageBitmap_PROM = checkImageBitmapOptionsSupport();
 const loadError = ref(false);
 const canvas = ref<HTMLCanvasElement>();
 const cwidth = ref(100);
@@ -34,21 +34,46 @@ let ctx2d: CanvasRenderingContext2D;
 
 
 
-onMounted(() => {
-  if( USE_ImageBitmap )
+onMounted(async () => {
+  if( await USE_ImageBitmap_PROM )
     ctxbm = canvas.value.getContext('bitmaprenderer');
   else
     ctx2d = canvas.value.getContext('2d');
   
-  watch(() => props.source, (val) => {
+  watch(() => props.source, async (val) => {
     loadError.value = false;
-    USE_ImageBitmap ? drawViaImageBitmap(val) : drawVia2Dcontext(val);
+    (await USE_ImageBitmap_PROM) ? drawViaImageBitmap(val) : drawVia2Dcontext(val);
   }, {immediate: true});
 });
 
 
 
 // functions
+
+async function checkImageBitmapOptionsSupport() {
+  if( typeof window.createImageBitmap !== 'function' )
+    return false;
+  
+  // in Firefox(142.0.1), createImageBitmap(image, sx, sy, sw, sh) does not function as expected.
+  if( /firefox/i.test(navigator.userAgent) )
+    return false;
+
+  // Make a 1x1 transparent ImageData
+  const data = new ImageData(1, 1);
+
+  try {
+    // try creating with 2 arguments: (ImageData, options)
+    // NOTE:
+    // certain versions of Firefox throw the following error
+    // when createImageBitmap is called with 2 arguments.
+    //   TypeError: Window.createImageBitmap: 2 is not a valid argument count for any overload.
+    const bmp = await createImageBitmap(data, { resizeWidth: 1 });
+    bmp.close(); // clean up
+    return true;
+  } catch {
+    return false;
+  }
+}
 
 async function drawViaImageBitmap(data: ImageBitmapSource) {
   try {
@@ -70,12 +95,13 @@ async function drawViaImageBitmap(data: ImageBitmapSource) {
     else
       ({width, height} = data);
 
-    const {tw, th, cropx: sx, cropy: sy, cropw: sw, croph: sh} = calcCoverSize(
+    const {tw, th, cropx, cropy, cropw, croph} = calcCoverSize(
       width, height, props.width, props.height
     );
 
-    const bitmap = await createImageBitmap(data, 
-      sx, sy, sw, sh,
+    const bitmap = await createImageBitmap(
+      data, 
+      cropx, cropy, cropw, croph,
       {
         resizeWidth: tw, resizeHeight: th,
         resizeQuality: 'pixelated',
@@ -97,19 +123,22 @@ async function drawViaImageBitmap(data: ImageBitmapSource) {
 }
 
 async function drawVia2Dcontext(data: ImageBitmapSource) {
+  let img: HTMLImageElement;
   if( data instanceof Blob ) {
     const url = URL.createObjectURL(data);
-    const img = new Image;
+    img = new Image;
     
     await new Promise(resolve => {
       img.onload = img.onerror = resolve;
       img.src = url;
       setTimeout(resolve, 10000);
+    }).finally(() => {
+      //img.onload = img.onerror = null;
     });
 
     if( !img.complete ) {
-      loadError.value = true;
-      return;
+      //loadError.value = true;
+      //return;
     }
 
     data = img;
@@ -127,8 +156,12 @@ async function drawVia2Dcontext(data: ImageBitmapSource) {
   canvas.value.height = th;
   canvas.value.style.width = tw + 'px';
   canvas.value.style.height = th + 'px';
-  console.log(calcCoverSize(width, height, props.width, props.height))
   ctx2d.drawImage(data, sx, sy, sw, sh);
+
+  if( img ) {
+    img.src = '';
+    img = null;
+  }
 }
 
 function calcCoverSize(w:number, h:number, tw:number, th:number) {
@@ -166,7 +199,10 @@ function calcCoverSize(w:number, h:number, tw:number, th:number) {
 <template>
   <canvas v-if="!loadError" ref="canvas" :width="cwidth" :height="cheight"></canvas>
   <div v-else class="error">
-    <svg xmlns="http://www.w3.org/2000/svg" :style="{width:Math.min(width, height)+'px'}" :height="Math.min(width, height)" viewBox="0 0 32 32">
+    <svg xmlns="http://www.w3.org/2000/svg"
+      :style="{width:Math.min(width, height)+'px'}"
+      :height="Math.min(width, height)" viewBox="0 0 32 32"
+    >
       <path fill="currentColor" d="M2 16A14 14 0 1 0 16 2A14 14 0 0 0 2 16m23.15 7.75L8.25 6.85a12 12 0 0 1 16.9 16.9M8.24 25.16a12 12 0 0 1-1.4-16.89l16.89 16.89a12 12 0 0 1-15.49 0"/>
     </svg>
   </div>
